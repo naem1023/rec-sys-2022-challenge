fb2b6bfb82aea7325d17bc96c0f429ce43d4749c
naem1023 <sungho-park@lotte.net>
2022-04-24 13:21:11 +0900

    Updaet script of merlin

--- run_merlin_container.sh
+++ run_merlin_container.sh
@@ -1,3 +1,3 @@
 docker run --gpus all --rm -it -p 8888:8888 -p 8797:8787 -p 8796:8786 --ipc=host \
-    -v ./transfo
+    -v `pwd`/dressipi_recsys2022:/workspace/recssys2022 -v `pwd`/merlin_dataset/:/workspace/data \
     nvcr.io/nvidia/merlin/merlin-pytorch-training:22.03 /bin/bash
